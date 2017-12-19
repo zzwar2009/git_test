7bb6ea1e4f15edc70e5f0bf1b4df7526d910ee1d
张铮 <zhengzhengzhang@easemob.com>
2017-12-19 11:07:41 +0800

    调整install
    Signed-off-by: 张铮 <zhengzhengzhang@easemob.com>

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 echo zhangzheng is learning travis ci ....z am ok
 CUR_SHA=`git rev-parse HEAD`
 CUR_TAG=`git tag --points-at $CUR_SHA`
-echo CUR_TAG
+echo $CUR_TAG
 if [ $CUR_TAG ] && [ "$CUR_TAG"x != ""x ]; then
     echo ''
     echo '[is a tag] start packing'
